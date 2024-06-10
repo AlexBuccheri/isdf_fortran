c048721df34cd3fdd45fe9e9f371b4535ad976bd
Alex Buccheri <alexanderbuccheri@googlemail.com>
2024-06-10 14:21:42 +0100

    Move all inversion drivers to this module, and unit test

--- src/lapack_drivers_m.F90
+++ src/lapack_drivers_m.F90
@@ -2,9 +2,172 @@ module lapack_drivers_m
     use, intrinsic :: iso_fortran_env, only: dp => real64
     implicit none
     private 
-    public :: qr_decomposition_with_pivot
+    public :: qr_decomposition_with_pivot, inversion_lu, pseudo_inv, svd
 
 contains
+
+   !> @bried Inversion of a matrix using LU decomposition
+   !! 
+   !! TODO Add checks for non-square matrices
+   !! Make a_inv optional, returnin the inverse in a
+   subroutine inversion_lu(a, a_inv)
+      real(dp)              :: a(:, :)      !< Matrix to inverse
+      real(dp), intent(out) :: a_inv(:, :)  !< Inverse(A), expected to be allocated memory by the caller
+
+      real(dp), allocatable :: tmp_a(:, :), work(:)
+      integer,  allocatable :: ipiv(:)
+      integer               :: m, n, info, lwork
+
+      m = size(a, 1)
+      n = size(a, 2)
+      allocate(tmp_a(m, n), source=a)
+      allocate(ipiv(min(m, n)))
+
+      ! LU factorization
+      call dgetrf(m, n, A, m, ipiv, info)
+
+      if (info /= 0) then
+         write(*,*) 'Error with getrf LU factorisation: ', info
+         stop
+      endif
+      
+      ! On exit, the factors L and U from the factorization
+      ! A = P*L*U; the unit diagonal elements of L are not stored 
+      ! Pass outputs (A and ipiv) directly to `getri`
+
+      ! Query workspace
+      allocate(work(1))
+      lwork = -1
+      call dgetri(n, A, m, ipiv, work, lwork, info)
+      lwork = int(work(1))
+      deallocate(work)
+
+      ! Compute inverse using LU decomposition
+      ! TODO See if this has an effect
+      !allocate(work(lwork))
+      allocate(work(n))
+      call dgetri(n, A, m, ipiv, work, size(work), info)
+      
+      if (info /= 0) then
+         write(*,*) 'Error with getri, inverting matrix A: ', info
+         stop
+      endif
+
+      deallocate(ipiv)
+
+      ! Return data in correct arrays (Should add a warning if not a square matrix)
+      a_inv = a
+      a = tmp_a
+      deallocate(tmp_a)
+
+   end subroutine inversion_lu
+   
+
+   !> @brief Compute pseudo-inv with SVD
+   ! TODO(Alex) See if A is preserved.
+   subroutine pseudo_inv(A, A_inv)
+      real(dp), intent(in)   :: A(:, :)
+      real(dp), intent(out)  :: A_inv(:, :)
+
+      real(dp), allocatable :: u(:, :), S(:), vt(:, :) !< Result of SVD on A
+      real(dp), allocatable :: s_inv(:, :)             !< inverse of the diagonals of S
+      real(dp), allocatable :: VSi(:, :)               !< Contraction of V and inverse(S)
+      integer :: i, m, n
+
+      call svd(A, U, S, VT)
+
+      ! Compute inverse of A: A^+ = V S^+ U^T
+      m = size(A, 1)
+      n = size(A, 2)
+
+      ! Invert the diagonals of S, to give S^+
+      ! TODO. Look into refactoring to see if I can use a vector instead of a diagonal matrix
+      allocate (s_inv(n, m), source=0._dp)
+      do i = 1, size(s, 1)
+         s_inv(i, i) = 1._dp/s(i)
+      end do
+      deallocate (S)
+
+      ! Contract (V S^+) = VS
+      allocate (VSi(n, n))
+      call dgemm('T', 'N', &
+                 size(VT, 2), &  ! Rows of op(A). op(A) = V
+                 size(s_inv, 2), &  ! Cols of op(B). op(B) = s_inv
+                 size(VT, 1), &  ! Cols of op(A)
+                 1._dp, &
+                 VT, &
+                 size(VT, 1), &  ! Rows of A
+                 s_inv, &
+                 size(s_inv, 1), &  ! Rows of B
+                 0._dp, &
+                 VSi, n)            ! Rows of C
+      deallocate (VT)
+
+      ! Contract A_inv = (V S^+) U^T
+      call dgemm('N', 'T', &
+                 size(VSi, 1), &    ! Rows of op(A). op(A) = VSi
+                 size(U, 1), &    ! Cols of op(B). op(B) = U^T
+                 size(VSi, 2), &    ! Cols of op(A)
+                 1._dp, &
+                 VSi, &
+                 size(VSi, 1), &    ! Rows of A
+                 U, &
+                 size(U, 1), &    ! Rows of B
+                 0._dp, &
+                 A_inv, m)          ! Rows of C
+
+      deallocate (VSi)
+      deallocate (U)
+
+   end subroutine pseudo_inv
+
+
+   !> @brief SVD wrapper
+   !!
+   !! Workspace query size taken from:
+   !! https://github.com/numericalalgorithmsgroup/LAPACK_Examples/blob/master/examples/source/dgesvd_example.f90
+   subroutine svd(A, U, S, VT)
+      real(dp), intent(in)  :: A(:, :)
+      real(dp), allocatable, intent(out) :: u(:, :)  !< U matrix
+      real(dp), allocatable, intent(out) :: S(:)     !<  min(m,n) singular values of A
+      real(dp), allocatable, intent(out) :: vt(:, :) !< V^T
+
+      real(dp), allocatable :: work(:)
+      real(dp) :: dummy(1, 1)
+      integer, parameter :: nb = 64
+      integer :: m, n, lda, ldu, ldvt, info, lwork
+
+      !                     A    =     U       S       VT
+      ! with shape:       (m,n)  =  (m, m)  (m, n)   (n, n)
+      ! which reduces to: (m,n)  =  (m, m) min(m, n) (n, n)
+      ! if S is represented as a vector.
+      m = size(A, 1)
+      n = size(A, 2)
+      lda = m
+      ldu = m
+      ldvt = n
+
+      allocate (s(min(m, n)))
+      allocate (u(ldu, m))
+      allocate (vt(ldvt, n))
+
+      ! Query optimal work space
+      lwork = -1
+      call dgesvd('A', 'S', m, n, A, lda, s, u, ldu, vt, ldvt, dummy, lwork, info)
+
+      ! Compute the singular values and left and right singular vectors of A.
+      lwork = max(m + 4*n + nb*(m + n), nint(dummy(1, 1)))
+      allocate (work(lwork))
+      call dgesvd('A', 'S', m, n, A, lda, s, u, ldu, vt, ldvt, work, lwork, info)
+
+      if (info /= 0) then
+         write (*, *) 'Failure in DGESVD. INFO =', info
+      end if
+
+      deallocate (work)
+
+   end subroutine svd
+
 
    !>@brief Perform QR decomposition with column-pivoting, on matrix A
    !!
@@ -50,6 +213,7 @@ contains
       ! Query the workspace
       allocate (work(1))
       lwork = -1
+      write(*, *) 'Workspace queried'
       call dgeqp3(n, m, A, n, p, tau, work, lwork, info)
 
       ! Allocate optimal work space
@@ -58,6 +222,7 @@ contains
       allocate (work(lwork))
 
       ! A P = Q R
+      write(*, *) 'Perform QR with pivoting'
       call dgeqp3(n, m, A, n, p, tau, work, lwork, info)
       deallocate (work)
 
